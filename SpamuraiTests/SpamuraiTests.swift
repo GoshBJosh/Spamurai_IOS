9a77c52bb8fab7ec3f18bce5c581167b957d502a
asdf <asdf@asdfs-imac.local>
2018-02-15 21:58:11 -0700

    Initial Commit

---
+++ SpamuraiTests/SpamuraiTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpamuraiTests.swift
+//  SpamuraiTests
+//
+//  Created by asdf on 2/15/18.
+//  Copyright © 2018 B'Gosh. All rights reserved.
+//
+
+import XCTest
+@testable import Spamurai
+
+class SpamuraiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
