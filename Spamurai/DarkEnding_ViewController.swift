d7c33fc9f19530a1640c6dc6680f642bd01fc69f
asdf <asdf@asdfs-imac.local>
2018-03-05 17:15:39 -0700

    .

---
+++ Spamurai/DarkEnding_ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DarkEnding_ViewController.swift
+//  Spamurai
+//
+//  Created by asdf on 2/26/18.
+//  Copyright © 2018 B'Gosh. All rights reserved.
+//
+
+import UIKit
+
+class DarkEnding_ViewController: UIViewController {
+
+    
+    
+    @IBAction func githubURL(_ sender: Any) {
+        if let url = NSURL(string:"http://github.com/GoshBJosh/Spamurai_IOS"){
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
---
+++ Spamurai/Main_ ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class Main_ViewController: UIViewController {
+    
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+
+}
+
